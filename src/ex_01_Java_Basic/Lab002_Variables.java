554f6f3f729fb8ace9c8790bb73e29a7cab53978
HimajaR94 <himajarayapudi@gmail.com>
2025-06-07 15:39:28 +0530

    Variables and Scannerclass sample code

---
+++ src/ex_01_Java_Basic/Lab002_Variables.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Lab002_Variables {
+    public static void main(String[] args) {
+
+        int num1=10;
+        int num2=20;
+        int result=num1+num2;
+        System.out.println(result);
+        boolean isAdult=true;
+        double pi=3.14;
+
+    }
+
+}
---
+++ src/ex_01_Java_Basic/Lab003_ScannerClass.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+
+import java.util.Scanner;
+
+public class Lab003_ScannerClass {
+    public static void main(String[] args) {
+
+        System.out.println("Enter first number:");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("Enter second number:");
+        int b=sc.nextInt();
+
+        System.out.println(a+b);
+
+    }
+
+}
